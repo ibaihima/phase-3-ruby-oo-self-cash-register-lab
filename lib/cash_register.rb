e0265c8284b1906ab3636a8fab4c4ee5f81b4e5b
Ibaihima <ibaihima@hotmail.com>
2022-03-21 10:26:57 -0400

    commit tree e335817c1e928eed3b8fa90c5c54991423216cc5

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,35 @@
+require 'pry'
+class CashRegister
+    attr_accessor :discount, :total, :quantity, :items
 
+    def initialize (discount = 0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def add_item(tittle,price,quantity=1)
+        @total += price * quantity
+        item = Hash["tittle" => tittle, "price" => price, "quantity" => quantity]
+        @items << item
+    end
+    def apply_discount
+        if self.discount != 0
+            disc = (self.discount.to_f / 100.0) * self.total
+            self.total = self.total - disc.to_i
+            "After the discount, the total comes to $#{self.total}."
+        else 
+            "There is no discount to apply."
+        end
+    end 
+    def items
+        cart = []
+        @items.map do |item|
+            items_in_cart = item["tittle"] * item["quantity"]
+            cart << items_in_cart
+        end 
+        cart
+    end 
+
+
+end
